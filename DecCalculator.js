03dc018776a306d29e9e956c17fd7c3b8fb199c7
RaisaChudek <61738648+RaisaChudek@users.noreply.github.com>
2020-05-22 15:13:35 +0100

    Add files via upload

---
+++ DecCalculator.js
@@ -0,0 +1,77 @@
+import Calculator from "./Calculator";
+
+
+class DecCalculator extends Calculator {
+  constructor(settings) {
+    super(settings);
+    console.log(this.getName());
+  }
+
+  changeNumber(root) {
+    let activeElement = root.querySelector(".active");
+
+    activeElement.setAttribute('contenteditable', true);
+    activeElement.addEventListener('input',function (event) {
+      if(event.data >= "0" && event.data < "9") {
+        event.target.innerText = event.data
+      } else {
+        event.target.innerText = 0;
+      }
+    })
+
+
+
+    /*[...activeElement.parentElement.children].forEach(el => {
+      if (el !== activeElement) {
+        el.classList.add("active");
+
+      }
+    })*/
+  };
+
+
+
+  add(numberX, numberY) {
+    let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = numberX.length - 1; i >= 0; i--) {
+      let carryBit = numberX[i] + numberY[i] + result[i];
+      if (carryBit >= 10) {
+        result[i] = carryBit - 10;
+        result[i - 1] = 1;
+      } else {
+        result[i] = carryBit;
+      }
+    }
+    return result;
+  }
+
+
+  initEvents()
+  {
+    super.initEvents();
+    let plus = this.$calculatorDOMElement.querySelector('.operator-bar')
+    console.log(plus);
+    plus.addEventListener('click',  (event)=> {
+      this.checkNumber();
+      this.updateResult();
+    })
+
+  }
+  updateResult() {
+    let root = this.$calculatorDOMElement;
+    let $resultNumber = root.querySelectorAll(".result-bit");
+
+    for (let i = this.resultNumberArray.length - 1, j = 0; i >= 0; i--, j++) {
+      ($resultNumber[j].querySelector(".active").innerText =this.resultNumberArray[i]);
+
+    }
+
+
+  }
+
+  /* Method changing number
+  */
+}
+
+
+export default DecCalculator;
